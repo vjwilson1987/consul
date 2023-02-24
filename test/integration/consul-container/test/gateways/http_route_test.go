7e4778b21fd1450161ec7a0b008f25f69ed7e8e1
Mike Morris <mikemorris@users.noreply.github.com>
2023-02-24 15:45:29 -0500

    remove EOF check, in CI this seems to sometimes be 'connection reset by peer' instead

--- test/integration/consul-container/test/gateways/http_route_test.go
+++ test/integration/consul-container/test/gateways/http_route_test.go
@@ -427,7 +427,7 @@ func TestHTTPRouteParentRefChange(t *testing.T) {
 	// check that second gateway does not resolve service
 	checkRouteError(t, address, gatewayTwoPort, "", map[string]string{
 		"Host": "test.example",
-	}, "EOF")
+	}, "")
 
 	// swtich route target to second gateway
 	route.Parents = []api.ResourceReference{
@@ -464,5 +464,5 @@ func TestHTTPRouteParentRefChange(t *testing.T) {
 	// check that first gateway has stopped resolving service
 	checkRouteError(t, address, gatewayOnePort, "", map[string]string{
 		"Host": "test.foo",
-	}, "EOF")
+	}, "")
 }
